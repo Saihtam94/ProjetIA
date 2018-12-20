ec345b1d45034880d1517d159867f3542899ffe4
Hugo Vincelet <hugo.vincelet@gmail.com>
2018-12-20 15:36:33 +0100

    ReadingMessages.py now provid e emails keywords and body text of every mail of a group of persons

--- script/ReadingMessages.py
+++ script/ReadingMessages.py
@@ -2,18 +2,43 @@
 from os import listdir
 from os.path import isfile, join, isdir
 
-folder = "../data/raw-data/maildir/"
-persons = ["allen-p", "arnold-j"]
+dbFolder = "../data/raw-data/maildir/"
 
-if 'persons' in globals():
-	if len(persons) == 0:
-		persons = [ name for name in listdir(folder) if isdir(join(folder, name)) ]
+persons = ["allen-p"]
+if len(persons) == 0:
+	persons = [ name for name in listdir(dbFolder) if isdir(join(dbFolder, name)) ]
 
-print(persons)
-for person in persons:
-	personMaildirFolder = join(folder, person)
-	if isdir(personMaildirFolder):
-		personCategoriesFolders = [f for f in listdir(personMaildirFolder) if isdir(join(personMaildirFolder, f))]
-		for category in personCategoriesFolders:
-			personOneCategoryFolder = join(personMaildirFolder, category)
-			personCategoryMailFiles = [f for f in listdir(personOneCategoryFolder) if isfile(join(personOneCategoryFolder, f))]
+def parseRawMessage(raw_message):
+	lines = raw_message.split('\n')
+	email = {"body":"", "from":"", "to":"", "subject":"", "cc":"", "bcc":""}
+	keys = ['from', 'to', 'subject', 'cc', 'bcc']
+	for line in lines:
+		if ':' not in line:
+			email["body"] += line.strip()
+		else:
+			pairs = line.split(':')
+			key = pairs[0].lower()
+			val = ""
+			for restOfKeysIndex in range(1, len(pairs)):
+				val += pairs[restOfKeysIndex].strip()
+			if key in keys:
+				email[key] = val
+	return email
+
+def dataPrepare(databasePath, persons):
+	for person in persons:
+		personMaildirFolder = join(databasePath, person)
+		if isdir(personMaildirFolder):
+			personCategoriesFolders = [f for f in listdir(personMaildirFolder) if isdir(join(personMaildirFolder, f))]
+			for category in personCategoriesFolders:
+				personOneCategoryFolder = join(personMaildirFolder, category)
+				personCategoryMailFiles = [f for f in listdir(personOneCategoryFolder) if isfile(join(personOneCategoryFolder, f))]
+				for filename in personCategoryMailFiles:
+					filepath = join(personOneCategoryFolder, filename)
+					file = open(filepath, "r")
+					oneLineCSV = parseRawMessage(file.read())
+					# outputWithoutStopWords = parseBody(oneLineCSV["body"])
+					# outputWithoutStopWordsWithStemming = parseBodyStemming(oneLineCSV["body"])
+					# outputWithoutStopWordsWithLemmatization = parseBodyLemmatization(oneLineCSV["body"])
+					# Add oneLineCSV in CSV file
+dataPrepare(dbFolder, persons)
