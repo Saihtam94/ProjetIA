f4aad17f8cb0b0e409987f8dd76e641cba38116e
Chloé Corfmat <chloe.corfmat@capgemini.com>
2019-02-05 09:18:42 +0100

    Update models script

--- script/data-scripts/PredictWithKnn.py
+++ script/data-scripts/PredictWithKnn.py
@@ -7,31 +7,51 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
+import pandas
 
 csvsPath = "../../data/csv-data/"
 
 emailsFileList = [f for f in listdir(csvsPath) if f.startswith("emails_")]
 for file in emailsFileList:
-	emailFilename = join(csvsPath, file)
-	csvFile = open(emailFilename, 'r', newline='')
-	fileReader = csv.reader(csvFile, delimiter=',')
-	dataframe = []
-	targets = []
-	next(fileReader)
-	for line in fileReader:
-		targets.append(line.pop())
-		dataframe.append(list(map(float, line)))
+	if isdir(join(csvsPath, file)):
+		dirname = join(csvsPath, file)
+		xtestfile = join(dirname, file) + '_x_test.csv'
+		xtestCsvFile = open(xtestfile, 'r', newline='')
+		fileReader = csv.reader(xtestCsvFile, delimiter=',')
 
-	# le = LabelEncoder()
-	# y = le.fit_transform(targets)
-	X_train, X_test, y_train, y_test = train_test_split(dataframe, targets, test_size=0.2, random_state=11)
+		X_test = []
+		for line in fileReader:
+			X_test.append(list(map(float, line)))
 
-	# KNN
-	neigh = KNeighborsClassifier(n_neighbors=4, weights="distance")
-	neigh.fit(X_train, y_train)
-	result = neigh.predict(X_test)
-	classif_report = classification_report(y_test, result)
-	print("\n### KNN for "+ file.split("_")[1] +" ###")
-	print(classif_report)
-	print("accuracy_score = " + str(accuracy_score(y_test, result)))
+		ytestfile = join(dirname, file) + '_y_test.csv'
+		ytestCsvFile = open(ytestfile, 'r', newline='')
+		fileReader = csv.reader(ytestCsvFile, delimiter=',')
+
+		y_test = []
+		for line in fileReader:
+			y_test.append(line.pop())
+
+		xtrainfile = join(dirname, file) + '_x_train.csv'
+		xtrainCsvFile = open(xtrainfile, 'r', newline='')
+		fileReader = csv.reader(xtrainCsvFile, delimiter=',')
+
+		X_train = []
+		for line in fileReader:
+			X_train.append(list(map(float, line)))
+
+		ytrainfile = join(dirname, file) + '_y_train.csv'
+		ytrainCsvFile = open(ytrainfile, 'r', newline='')
+		fileReader = csv.reader(ytrainCsvFile, delimiter=',')
+
+		y_train = []
+		for line in fileReader:
+			y_train.append(line.pop())
+
+		neigh = KNeighborsClassifier(n_neighbors=4, weights="distance")
+		neigh.fit(X_train, y_train)
+		result = neigh.predict(X_test)
+		classif_report = classification_report(y_test, result)
+		print("\n### KNN for "+ file.split("_")[1] +" ###")
+		print(classif_report)
+		print("accuracy_score = " + str(accuracy_score(y_test, result)))
 		
--- script/data-scripts/PredictWithNB.py
+++ script/data-scripts/PredictWithNB.py
@@ -12,26 +12,45 @@ csvsPath = "../../data/csv-data/"
 
 emailsFileList = [f for f in listdir(csvsPath) if f.startswith("emails_")]
 for file in emailsFileList:
-	emailFilename = join(csvsPath, file)
-	csvFile = open(emailFilename, 'r', newline='')
-	fileReader = csv.reader(csvFile, delimiter=',')
-	dataframe = []
-	targets = []
-	next(fileReader)
-	for line in fileReader:
-		targets.append(line.pop())
-		dataframe.append(list(map(float, line)))
+	if isdir(join(csvsPath, file)):
+		dirname = join(csvsPath, file)
+		xtestfile = join(dirname, file) + '_x_test.csv'
+		xtestCsvFile = open(xtestfile, 'r', newline='')
+		fileReader = csv.reader(xtestCsvFile, delimiter=',')
 
-	# le = LabelEncoder()
-	# y = le.fit_transform(targets)
-	X_train, X_test, y_train, y_test = train_test_split(dataframe, targets, test_size=0.2, random_state=11)
+		X_test = []
+		for line in fileReader:
+			X_test.append(list(map(float, line)))
 
-	# KNN
-	gnb = GaussianNB()
-	neigh = gnb.fit(X_train, y_train)
-	result = neigh.predict(X_test)
-	classif_report = classification_report(y_test, result)
-	print("\n### Naive Bayes for "+ file.split("_")[1] +" ###")
-	print(classif_report)
-	print("accuracy_score = " + str(accuracy_score(y_test, result)))
+		ytestfile = join(dirname, file) + '_y_test.csv'
+		ytestCsvFile = open(ytestfile, 'r', newline='')
+		fileReader = csv.reader(ytestCsvFile, delimiter=',')
+
+		y_test = []
+		for line in fileReader:
+			y_test.append(line.pop())
+
+		xtrainfile = join(dirname, file) + '_x_train.csv'
+		xtrainCsvFile = open(xtrainfile, 'r', newline='')
+		fileReader = csv.reader(xtrainCsvFile, delimiter=',')
+
+		X_train = []
+		for line in fileReader:
+			X_train.append(list(map(float, line)))
+
+		ytrainfile = join(dirname, file) + '_y_train.csv'
+		ytrainCsvFile = open(ytrainfile, 'r', newline='')
+		fileReader = csv.reader(ytrainCsvFile, delimiter=',')
+
+		y_train = []
+		for line in fileReader:
+			y_train.append(line.pop())
+
+		gnb = GaussianNB()
+		neigh = gnb.fit(X_train, y_train)
+		result = neigh.predict(X_test)
+		classif_report = classification_report(y_test, result)
+		print("\n### Naive Bayes for "+ file.split("_")[1] +" ###")
+		print(classif_report)
+		print("accuracy_score = " + str(accuracy_score(y_test, result)))
 		
